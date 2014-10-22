9164ae4c0b0796c3310ff9db8819a01cc3164b9d
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-10-22 12:39:45 -0200

    Fix: features tests with new fields names

--- spec/features/create_tasks_spec.rb
+++ spec/features/create_tasks_spec.rb
@@ -5,8 +5,8 @@ feature "Create Tasks" do
 	before :each do
 		@user = create(:user)
 		visit new_user_session_path
-    fill_in "Email", :with => @user.email
-    fill_in "Password", :with => @user.password
+    fill_in "E-mail", :with => @user.email
+    fill_in "Senha", :with => @user.password
     click_button "Log in"
 	end
 
