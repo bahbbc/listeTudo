084cba1b96efecb9bedf93be2b25a1e2e198d866
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-10-22 10:31:46 -0200

    Fix spec/models/task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Task, :type => :model do
 			task1 = create(:task, list: list)
 			task2 = create(:task, list: list)
 			task3 = create(:task, list: list, status: true)
-			expect(Task.undone_order).to eq [task1, task2, task3]
+			expect(Task.undone_order).to eq [task2, task1, task3]
 		end
 	end
 
