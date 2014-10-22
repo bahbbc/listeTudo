aff6a7efc55f724689ecfa16b5bc0e37df2fb402
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-10-22 01:17:32 -0200

    Add tests to undone_order

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe List, :type => :model do
+RSpec.describe Task, :type => :model do
+
+	describe '#undone_order' do
+		it 'returns tasks order by the uncomplete tasks' do
+			list = create(:list)
+			task1 = create(:task, list: list)
+			task2 = create(:task, list: list)
+			task3 = create(:task, list: list, status: true)
+			expect(Task.undone_order).to eq [task1, task2, task3]
+		end
+	end
 
 end
